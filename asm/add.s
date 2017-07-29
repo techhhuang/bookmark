7c0d07ad32e52ac9438e41ec424291a66eee1266
techhuang <techhuang@163.com>
2017-07-29 17:17:32 +0800

    update

---
+++ asm/add.s
@@ -0,0 +1,24 @@
+	.file	"add.c"
+	.text
+	.globl	add
+	.type	add, @function
+add:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	8(%ebp), %edx
+	movl	12(%ebp), %eax
+	addl	%edx, %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add, .-add
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
--- ui/main.c
+++ ui/main.c
@@ -87,7 +87,7 @@ static void init_egl()
     int i;
     EGLint config_attribs[] = {EGL_SURFACE_TYPE, EGL_WINDOW_BIT, EGL_RED_SIZE, 8,
                                EGL_GREEN_SIZE, 8, EGL_BLUE_SIZE, 8,
-                               EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_NONE
+                               EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT, EGL_NONE
                               };
 
     static const EGLint context_attribs[] = {EGL_CONTEXT_CLIENT_VERSION, 2,
